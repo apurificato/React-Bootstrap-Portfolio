af7469d7b2ca608a3026c87564956fe9fbbf4097
Tony Stark <apurificato5680@gmai.com>
2024-06-06 12:20:05 -0400

    started adding all of the pages and components that I need on the site and styling them - header, footer, nav, contactme, resume, aboutme. still need to add a work page and continue styling.

---
+++ client/src/pages/Resume.jsx
@@ -0,0 +1,31 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Resume() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="resume">
+          <h2>My Resume</h2>
+          <h3>Click to Download and Save My Resume For Your Reference</h3>
+          <div id="download-section">
+            <a
+              href="/Anthony-Purificato-Web-Developer-Resume-2024.pdf"
+              download="AP-Resume"
+            >
+              <button type="button">Download Resume</button>
+            </a>
+          </div>
+          <h5>
+            Thank you for visiting my site. I look forward to connecting with
+            you soon!
+          </h5>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Resume;
