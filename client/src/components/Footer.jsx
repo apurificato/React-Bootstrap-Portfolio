1eca2795fd92844820a859d653938e1bf04b5aa4
Tony Stark <apurificato5680@gmai.com>
2024-06-08 23:47:24 -0400

    updated Work page and all of the responsive issues that were occurring with elements in project item cards and the technology icon sizing and flex properties. Also fixed the button spacing issues. Updated footer elements and properties. Had to add more Media Queries to resolve some issues with the smaller screen sizes.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,19 +3,19 @@ function Footer() {
     <footer className="footer">
       <div className="brand">
         <p>&copy;Copyright 2024</p>
-        <div className="footer-icons">
-          <a href="https://www.github.com/apurificato">
-            <img
-              className="footer-logo-item brand-logo"
-              src="/Github-logo.svg"
-            ></img>
-          </a>
-          <a href="https://www.linkedin.com/in/apurificato">
-            <img
-              className="footer-logo-item brand-logo"
-              src="/LinkedIn-logo.svg"
-            ></img>
-          </a>
+        <div className="d-flex flex-row justify-content-between footer-icons">
+            <a href="https://www.github.com/apurificato">
+              <img
+                className="footer-logo-item brand-logo"
+                src="/Github-logo.svg"
+              ></img>
+            </a>
+            <a href="https://www.linkedin.com/in/apurificato">
+              <img
+                className="footer-logo-item brand-logo"
+                src="/LinkedIn-logo.svg"
+              ></img>
+            </a>
           {/* <a href='https://www.instagram.com/tonypreetz'>
                 <img className="footer-logo-item" src='/Instagram-logo.svg'></img>
             </a> */}
